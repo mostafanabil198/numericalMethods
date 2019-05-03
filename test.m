dc486fe39bac046c92ea1aa91f6348500c66f373
mostafa nabil <mostafanabil198@gmail.com>
2019-05-03 18:05:37 +0200

    add write to file and change some table returns

--- test.m
+++ test.m
@@ -1,22 +1,8 @@
+[ table,root, time, errorMsg ] = Secant('x^2 + x - 5', 1, 3, 10, .00001, 'false');
 
+%write table
+ %dlmwrite('m1.txt',table)
+ 
+ WriteToFile(table,root,time,errorMsg,'Secant');
 
-fid=fopen('m.txt');
-tline = fgetl(fid);
-tlines = cell(0,1);
-while ischar(tline)
-    tlines{end+1,1} = tline;
-    tline = fgetl(fid);
-end
-isaninteger = @(x) ~isinf(x) & floor(x) == x;
-syms x;
-f(x) = sym(tlines{1});
-method = tlines{2};
-interval = tlines{3};
-itrOrEps = str2double(tlines{4});
-if(floor(itrOrEps) == itrOrEps)
-   itr = itrOrEps
-else
-    eps = itrOrEps
-end
-
-fclose(fid);
+ 
