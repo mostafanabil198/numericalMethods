492f7656bda414a92beef18d8b5973e8c9013e56
mostafa nabil <mostafanabil198@gmail.com>
2019-05-15 04:13:07 +0200

    add general algorithm to files and gui

--- checkMultiplicity.m
+++ checkMultiplicity.m
@@ -2,13 +2,13 @@
 function [m]= checkMultiplicity(f,root,precision)
     syms x;
     der = diff(f, x);
-    m = [];
+    m = 1;
     while der ~= 0
         val = vpa(subs(der,root));
         disp(der);
         if abs(val) < precision
-            m = [m; root];
-            der = diff(der,x);
+            m = m+1
+            der = diff(der,x)
         else
             break;
         end
